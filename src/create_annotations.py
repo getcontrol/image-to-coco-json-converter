e04a83b377f81665dc6fdda16d0321879b4c7f73
getcontrol <loewen007@gmail.com>
2021-07-01 15:40:19 -0700

    Create_annotations.py

--- src/create_annotations.py
+++ src/create_annotations.py
@@ -1,9 +1,14 @@
-from PIL import Image                                      # (pip install Pillow)
-import numpy as np                                         # (pip install numpy)
-from skimage import measure                                # (pip install scikit-image)
-from shapely.geometry import Polygon, MultiPolygon         # (pip install Shapely)
+# (pip install Pillow)
+from PIL import Image
+# (pip install numpy)
+import numpy as np
+# (pip install scikit-image)
+from skimage import measure
+# (pip install Shapely)
+from shapely.geometry import Polygon, MultiPolygon
 import os
 import json
+
 
 def create_sub_masks(mask_image, width, height):
     # Initialize a dictionary of sub-masks indexed by RGB colors
@@ -11,7 +16,7 @@ def create_sub_masks(mask_image, width, height):
     for x in range(width):
         for y in range(height):
             # Get the RGB values of the pixel
-            pixel = mask_image.getpixel((x,y))[:3]
+            pixel = mask_image.getpixel((x, y))[:3]
 
             # Check to see if we have created a sub-mask...
             pixel_str = str(pixel)
@@ -21,18 +26,20 @@ def create_sub_masks(mask_image, width, height):
                 # Note: we add 1 pixel of padding in each direction
                 # because the contours module doesn"t handle cases
                 # where pixels bleed to the edge of the image
-                sub_masks[pixel_str] = Image.new("1", (width+2, height+2))
+                sub_masks[pixel_str] = Image.new("1", (width + 2, height + 2))
 
             # Set the pixel value to 1 (default is 0), accounting for padding
-            sub_masks[pixel_str].putpixel((x+1, y+1), 1)
+            sub_masks[pixel_str].putpixel((x + 1, y + 1), 1)
 
     return sub_masks
+
 
 def create_sub_mask_annotation(sub_mask):
     # Find contours (boundary lines) around each sub-mask
     # Note: there could be multiple contours if the object
     # is partially occluded. (E.g. an elephant behind a tree)
-    contours = measure.find_contours(np.array(sub_mask), 0.5, positive_orientation="low")
+    contours = measure.find_contours(
+        np.array(sub_mask), 0.5, positive_orientation="low")
 
     polygons = []
     segmentations = []
@@ -44,19 +51,26 @@ def create_sub_mask_annotation(sub_mask):
             contour[i] = (col - 1, row - 1)
 
         # Make a polygon and simplify it
-        poly = Polygon(contour)
-        poly = poly.simplify(1.0, preserve_topology=False)
+            poly = Polygon(contour)
+            poly = poly.simplify(1.0, preserve_topology=False)
 
-        if(poly.is_empty):
-            # Go to next iteration, dont save empty values in list
+            if(poly.is_empty):
+                    # Go to next iteration, dont save empty values in list
+                    continue
+
+        if (poly.area > 16):  # Ignore tiny polygons
+            if (poly.geom_type == 'MultiPolygon'):
+                # if MultiPolygon, take the smallest convex Polygon containing all the points in the object
+                poly = poly.convex_hull
+        if (poly.geom_type == 'Polygon'):  # Ignore if still not a Polygon (could be a line or point)
+            polygons.append(poly)
+            segmentation = np.array(poly.exterior.coords).ravel().tolist()
+            segmentations.append(segmentation)
+        else:
             continue
 
-        polygons.append(poly)
-
-        segmentation = np.array(poly.exterior.coords).ravel().tolist()
-        segmentations.append(segmentation)
-
     return polygons, segmentations
+
 
 def create_category_annotation(category_dict):
     category_list = []
@@ -71,6 +85,7 @@ def create_category_annotation(category_dict):
 
     return category_list
 
+
 def create_image_annotation(file_name, width, height, image_id):
     images = {
         "file_name": file_name,
@@ -80,6 +95,7 @@ def create_image_annotation(file_name, width, height, image_id):
     }
 
     return images
+
 
 def create_annotation_format(polygon, segmentation, image_id, category_id, annotation_id):
     min_x, min_y, max_x, max_y = polygon.bounds
@@ -100,8 +116,9 @@ def create_annotation_format(polygon, segmentation, image_id, category_id, annot
 
     return annotation
 
+
 def get_coco_json_format():
-    # Standard COCO format 
+    # Standard COCO format
     coco_format = {
         "info": {},
         "licenses": [],
